338868ca43623cde80a300fbbb3c34fa0d953268
Humberto Domínguez <humbertodelac13@gmail.com>
2024-02-19 09:37:00 -0600

    modelos agregados

---
+++ app/Models/proveedoresModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class proveedoresModel extends Model
+{
+    use HasFactory;
+}
