cc03dafb18cd53064ab172ce7c8f416a6f1e70b0
Humberto Domínguez <humbertodelac13@gmail.com>
2024-02-19 10:16:30 -0600

    migraciones actualizadas

--- database/migrations/2024_02_19_152636_create_productos_table.php
+++ database/migrations/2024_02_19_152636_create_productos_table.php
@@ -4,15 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('productos', function (Blueprint $table) {
-            $table->id();
+            $table->increments('idProducto');
+            $table->string('nombre', 100);
+            $table->string('descripcion');
+            $table->double('precio');
+            $table->date('expiracion');
+            $table->integer('stock');
+            $table->integer('idProveedor')->unsigned();
+            //indica que idProveedor es una llave foranea, referencia a la comuna idProveedor de la tabla proveedores
+            $table->foreign('idProveedor')->references('idProveedor')->on('proveedores');
             $table->timestamps();
         });
     }
